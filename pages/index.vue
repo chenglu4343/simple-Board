80ca2d0792ca3bf3c62867fbdd7335b8014f1bfa
成路 <48679486+chenglu4343@users.noreply.github.com>
2023-04-14 07:46:06 +0000

    🎉 init: nuxt

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { NCard } from 'naive-ui'
+
+const msg = ref('Hello World')
+</script>
+
+<template>
+  <div class="text-red">
+    {{ msg }}
+  </div>
+  <NCard>Hello</NCard>
+</template>
