d2f33fc5bd48a4fc59fe8989be5caa8605c1b48e
成路 <chenglu4343@qq.com>
2023-04-14 09:05:40 +0000

    ✨ feat: 添加清单模式下添加task逻辑

--- pages/index.vue
+++ pages/index.vue
@@ -4,22 +4,33 @@ import { useListMode } from './composables/useListMode'
 import type { List } from '~/types'
 import { createList } from '~/utils/createType'
 
-const { isBoard, handleSwithModeChange } = useListMode()
+const { isBoard, isList, handleSwithModeChange } = useListMode()
 
 const title = ref('')
 const list = ref<List>(createList())
 
 function handleTitleInputEnter() {
+  if (!title.value)
+    return
 
+  addTask2List(list.value, createTask(title.value))
+  title.value = ''
 }
 </script>
 
 <template>
   <main class="p-2">
     <header>
-      {{ isBoard ? '看板模式' : '清单模式' }}<NSwitch :value="isBoard" :on-update-value="handleSwithModeChange" />
+      {{ isBoard ? '看板模式' : '清单模式' }}
+      <NSwitch :value="isBoard" :on-update-value="handleSwithModeChange" />
     </header>
 
-    <NInput v-model:value="title" placeholder="输入标题" class="mt-2" @enter="handleTitleInputEnter" />
+    <NInput v-model:value="title" placeholder="输入标题" class="mt-2" @keyup.enter="handleTitleInputEnter" />
+
+    <template v-if="isList && list.groups.length === 1">
+      <div v-for="(task, index) of list.groups[0].tasks" :key="index">
+        {{ task.title }}
+      </div>
+    </template>
   </main>
 </template>
