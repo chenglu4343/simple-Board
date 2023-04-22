6bb679fc3d523c91d88562a4e1664fb8deff55e5
chenglu <chenglu4343@qq.com>
2023-04-22 21:32:04 +0800

    ✨ feat: add Task Component add todo status toggle

---
+++ components/Task.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { NCheckbox } from 'naive-ui'
+import type { Task } from '~/types'
+
+const props = defineProps<{
+  task: Task
+}>()
+
+const emits = defineEmits<{
+  (e: 'update:task', task: Task): void
+}>()
+
+function handleTaskStatusChange(val: boolean) {
+  emits('update:task', {
+    ...props.task,
+    status: val ? 'done' : 'todo',
+  })
+}
+</script>
+
+<template>
+  <div>
+    <NCheckbox :checked="task.status === 'done'" @update:checked="handleTaskStatusChange" />
+    <span>
+      {{ task.title }}
+    </span>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
 import { NInput, NSwitch } from 'naive-ui'
 import { useListMode } from './composables/useListMode'
-import type { List } from '~/types'
+import type { List, Task } from '~/types'
 import { createList } from '~/utils/createType'
+import TaskCom from '~/components/Task.vue'
 
 const { isBoard, isList, handleSwithModeChange } = useListMode()
 
-const title = ref('')
+const titleInput = ref('')
 const list = ref<List>(createList())
 
 function handleTitleInputEnter() {
-  if (!title.value)
+  if (!titleInput.value)
     return
 
-  addTask2List(list.value, createTask(title.value))
-  title.value = ''
+  addTask2List(list.value, createTask(titleInput.value))
+  titleInput.value = ''
+}
+
+function handleTaskChange(task: Task, index: number) {
+  list.value.groups[0].tasks[index] = task
 }
 </script>
 
@@ -25,12 +30,20 @@ function handleTitleInputEnter() {
       <NSwitch :value="isBoard" :on-update-value="handleSwithModeChange" />
     </header>
 
-    <NInput v-model:value="title" placeholder="输入标题" class="mt-2" @keyup.enter="handleTitleInputEnter" />
+    <NInput
+      v-model:value="titleInput"
+      placeholder="输入标题"
+      class="mt-2"
+      @keyup.enter="handleTitleInputEnter"
+    />
 
     <template v-if="isList && list.groups.length === 1">
-      <div v-for="(task, index) of list.groups[0].tasks" :key="index">
-        {{ task.title }}
-      </div>
+      <TaskCom
+        v-for="(task, index) of list.groups[0].tasks"
+        :key="index"
+        :task="task"
+        @update:task="(val) => handleTaskChange(val, index)"
+      />
     </template>
   </main>
 </template>
