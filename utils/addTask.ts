096f82b3f0e5250555ac9f2070ac598f73cc4794
chenglu <chenglu4343@qq.com>
2023-04-22 23:15:02 +0800

    ✨ feat: add task in first group

--- utils/addTask.ts
+++ utils/addTask.ts
@@ -5,5 +5,5 @@ export function addTask2List(list: List, task: Task) {
   if (list.groups.length === 0)
     list.groups.push(createGroup())
 
-  list.groups.at(-1)?.tasks.unshift(task)
+  list.groups[0]?.tasks.unshift(task)
 }
