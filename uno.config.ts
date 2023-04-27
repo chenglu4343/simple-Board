09fa5532ed90c992ca4175ba39ea27db355b2bb7
成路 <chenglu4343@qq.com>
2023-04-27 08:44:54 +0000

    🐳 chore: add icon support

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,17 @@
 // uno.config.ts
-import { defineConfig, presetUno } from 'unocss'
+import { defineConfig, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
   // ...UnoCSS options
   presets: [
     presetUno(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
+      scale: 1.2,
+      warn: true,
+    }),
   ],
 })
