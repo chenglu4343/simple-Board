318ec049af18fc0028618d75ff9344b03f5fde6d
成路 <chenglu4343@qq.com>
2023-04-27 09:08:31 +0000

    ✨ feat: add group more btn view

--- components/Group.vue
+++ components/Group.vue
@@ -23,7 +23,38 @@ function handleAddTask(task: TaskType) {
 
 <template>
   <div class="bg-white p-2 rounded grid grid-rows-[auto_auto_1fr] gap-2 max-h-full box-border">
-    <div>{{ group.title }}</div>
+    <div class="flex justify-between">
+      <span>{{ group.title }}</span>
+      <NPopover
+        trigger="click" :theme-overrides="{
+          padding: '2px 5px',
+        }"
+      >
+        <template #trigger>
+          <div class="i-ant-design:more-outlined cursor-pointer" />
+        </template>
+        <template #default>
+          <ul class="list-none p-0 cursor-pointer">
+            <li class="hover:bg-gray-1">
+              <div class="i-ant-design:edit-filled" />
+              <span>重命名</span>
+            </li>
+            <li class="hover:bg-gray-1">
+              <div class="i-ant-design:insert-row-left-outlined" />
+              <span>左侧添加分组</span>
+            </li>
+            <li class="hover:bg-gray-1">
+              <div class="i-ant-design:insert-row-right-outlined" />
+              <span>右侧添加分组</span>
+            </li>
+            <li class="hover:bg-gray-1">
+              <div class="i-ant-design:delete-filled" />
+              <span>删除</span>
+            </li>
+          </ul>
+        </template>
+      </NPopover>
+    </div>
     <TaskInput @add-task="handleAddTask" />
     <TaskList :tasks="group.tasks" :group="taskListGroup" class="m-h-0 overflow-y-scroll" @update:tasks="(val) => handleTasksChange(val)" />
   </div>
