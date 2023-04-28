9a88d49c6c8cbeb29bb95b6fbe48214271f04e89
成路 <chenglu4343@qq.com>
2023-04-28 03:10:16 +0000

    ✨ feat: enter完成编辑

--- components/FocusInput/index.vue
+++ components/FocusInput/index.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import type { InputInst, InputProps } from 'naive-ui'
+import { mergeProps } from 'vue'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   inputProps?: InputProps
   isEdit?: boolean
-}>()
+  isEnterEndEdit?: boolean
+}>(), {
+  isEnterEndEdit: true,
+})
 
 const emits = defineEmits<{
   (e: 'update:isEdit', val: boolean): void
@@ -15,10 +19,11 @@ defineOptions({
 })
 
 const inputRef = ref<InputInst | null>(null)
-
-function handleBlur() {
-  emits('update:isEdit', false)
-}
+const computedInputProps = computed(() =>
+  mergeProps(props.inputProps || {}, {
+    'onBlur': () => emits('update:isEdit', false),
+    'onKeyup.enter': () => { props.isEnterEndEdit && emits('update:isEdit', false) },
+  }))
 
 watchEffect(() => {
   if (props.isEdit)
@@ -28,5 +33,10 @@ watchEffect(() => {
 
 <template>
   <span v-if="!isEdit">{{ inputProps?.value }}</span>
-  <NInput v-else ref="inputRef" v-bind="{ ...inputProps }" @blur="handleBlur" />
+  <NInput
+    v-else
+    ref="inputRef"
+    v-bind="computedInputProps"
+    @keyup.enter="computedInputProps['onKeyup.enter']"
+  />
 </template>
