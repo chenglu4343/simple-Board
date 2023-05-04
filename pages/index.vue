f5d6807431b19074fe6278f372abf14d2632b10c
chenglu <chenglu4343@qq.com>
2023-05-04 21:32:28 +0800

    ✨ feat: 添加侧边栏

--- pages/index.vue
+++ pages/index.vue
@@ -36,73 +36,81 @@ function handleSwitchModeChange(val: boolean) {
 </script>
 
 <template>
-  <main
-    class="p-2 h-100vh box-border bg-gray-1 grid gap-2" :class="{
-      'grid-rows-[auto_auto_1fr]': isList,
-      'grid-rows-[auto_1fr]': isBoard,
-    }"
-  >
-    <header>
-      {{ isBoard ? '看板模式' : '清单模式' }}
-      <NSwitch :value="isBoard" :on-update-value="handleSwitchModeChange" />
-    </header>
+  <NLayout has-sider>
+    <NLayoutSider show-trigger="arrow-circle" bordered collapse-mode="transform">
+      这是个清单列表
+    </NLayoutSider>
 
-    <TaskInput v-if="isList" @add-task="handleAddTask" />
-
-    <template v-if="isList && list.groups.length === 1">
-      <TaskList v-model:task-ids="list.groups[0].taskIds" />
-    </template>
-
-    <template v-else-if="isList && list.groups.length > 1">
-      <Draggable
-        v-model="list.groups"
-        class="overflow-y-scroll"
-        item-key="index" tag="n-collapse"
-        :component-data="{
-          themeOverrides: {
-            dividerColor: '#6B6B77FF',
-          },
-          defaultExpandedNames: list.groups.map((item, index) => `${index}-${item.title}`),
+    <NLayoutContent>
+      <main
+        class="p-2 h-100vh box-border bg-gray-1 grid gap-2" :class="{
+          'grid-rows-[auto_auto_1fr]': isList,
+          'grid-rows-[auto_1fr]': isBoard,
         }"
       >
-        <template #item="{ element, index }">
-          <CollapseGroup
-            :group="element"
-            preset="operate-group"
-            task-list-group="collaspe-group"
-            :collapse-item-name="`${index}-${element.title}`"
-            @update:group="(val) => handleGroupChange(val, index)"
-            @insert-top-group="handleInsertLeftGroup(index)"
-            @insert-bottom-group="handleInsertRightGroup(index)"
-            @delete-group="handleDeleteGroup(index)"
-          />
-        </template>
-      </Draggable>
-    </template>
+        <header>
+          {{ isBoard ? '看板模式' : '清单模式' }}
+          <NSwitch :value="isBoard" :on-update-value="handleSwitchModeChange" />
+        </header>
 
-    <template v-else-if="isBoard">
-      <Draggable
-        v-model="list.groups"
-        class="flex items-start gap-2 flex-nowrap overflow-x-scroll scrollbar"
-        item-key="index"
-      >
-        <template #item="{ element, index }">
-          <Group
-            :group="element"
-            task-list-group="list-group"
-            class="min-w-60 max-w-60"
-            @update:group="(val) => handleGroupChange(val, index)"
-            @insert-left-group="handleInsertLeftGroup(index)"
-            @insert-right-group="handleInsertRightGroup(index)"
-            @delete-group="handleDeleteGroup(index)"
-          />
+        <TaskInput v-if="isList" @add-task="handleAddTask" />
+
+        <template v-if="isList && list.groups.length === 1">
+          <TaskList v-model:task-ids="list.groups[0].taskIds" />
         </template>
-        <template #footer>
-          <NButton type="primary" @click="handleAddGroup">
-            添加分组
-          </NButton>
+
+        <template v-else-if="isList && list.groups.length > 1">
+          <Draggable
+            v-model="list.groups"
+            class="overflow-y-scroll"
+            item-key="index" tag="n-collapse"
+            :component-data="{
+              themeOverrides: {
+                dividerColor: '#6B6B77FF',
+              },
+              defaultExpandedNames: list.groups.map((item, index) => `${index}-${item.title}`),
+            }"
+          >
+            <template #item="{ element, index }">
+              <CollapseGroup
+                :group="element"
+                preset="operate-group"
+                task-list-group="collaspe-group"
+                :collapse-item-name="`${index}-${element.title}`"
+                @update:group="(val) => handleGroupChange(val, index)"
+                @insert-top-group="handleInsertLeftGroup(index)"
+                @insert-bottom-group="handleInsertRightGroup(index)"
+                @delete-group="handleDeleteGroup(index)"
+              />
+            </template>
+          </Draggable>
         </template>
-      </Draggable>
-    </template>
-  </main>
+
+        <template v-else-if="isBoard">
+          <Draggable
+            v-model="list.groups"
+            class="flex items-start gap-2 flex-nowrap overflow-x-scroll scrollbar"
+            item-key="index"
+          >
+            <template #item="{ element, index }">
+              <Group
+                :group="element"
+                task-list-group="list-group"
+                class="min-w-60 max-w-60"
+                @update:group="(val) => handleGroupChange(val, index)"
+                @insert-left-group="handleInsertLeftGroup(index)"
+                @insert-right-group="handleInsertRightGroup(index)"
+                @delete-group="handleDeleteGroup(index)"
+              />
+            </template>
+            <template #footer>
+              <NButton type="primary" @click="handleAddGroup">
+                添加分组
+              </NButton>
+            </template>
+          </Draggable>
+        </template>
+      </main>
+    </NLayoutContent>
+  </NLayout>
 </template>
