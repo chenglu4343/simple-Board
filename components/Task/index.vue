6a8d8c6d388ad3ea576445476e0f71ba4487123c
chenglu <chenglu4343@qq.com>
2023-05-04 21:15:57 +0800

    ✨ feat: task添加右键删除

--- components/Task/index.vue
+++ components/Task/index.vue
@@ -16,6 +16,9 @@ defineOptions({
 })
 
 const isShowEditDrawer = ref(false)
+const isShowToolPopover = ref(false)
+const toolPopoverX = ref(0)
+const toolPopoverY = ref(0)
 
 async function handleUpdateTask(newTask: TaskType) {
   await dbService.updateTask(newTask)
@@ -33,18 +36,46 @@ async function handleTaskStatusChange(val: boolean) {
 function handleEditTask() {
   isShowEditDrawer.value = true
 }
+
+function handleRightClickTitle(e: MouseEvent) {
+  toolPopoverX.value = e.clientX
+  toolPopoverY.value = e.clientY
+  isShowToolPopover.value = true
+}
+
+function handleDeleteTask() {
+  // TODO:list的数据存储做好后再做
+}
 </script>
 
 <template>
-  <div class="cursor-pointer" @click="handleEditTask">
+  <div class="cursor-pointer grid grid-cols-[auto_1fr] gap-2" @click="handleEditTask">
     <NCheckbox
       :checked="task.status === 'done'"
       @update:checked="handleTaskStatusChange"
       @click.stop="() => {}"
     />
-    <span>
+
+    <span @click.prevent.right="handleRightClickTitle">
       {{ task.title }}
     </span>
+
+    <NPopover
+      v-model:show="isShowToolPopover"
+      trigger="manual"
+      :x="toolPopoverX"
+      :y="toolPopoverY"
+      placement="bottom"
+      :theme-overrides="{
+        padding: '2px 5px',
+      }"
+      @clickoutside="isShowToolPopover = false"
+    >
+      <div class="flex gap-2 p-2 items-center cursor-pointer" @click="handleDeleteTask">
+        <div class="i-ant-design:delete-filled" />
+        <span>删除</span>
+      </div>
+    </NPopover>
 
     <TaskEditDrawer v-model:show="isShowEditDrawer" :task="task" @update:task="handleUpdateTask" />
   </div>
