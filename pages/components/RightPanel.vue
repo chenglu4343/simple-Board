293e8e0549bdfc5abff8fb79cbdc916fd2789730
成路 <chenglu4343@qq.com>
2023-05-05 07:42:08 +0000

    ✨ feat: list存入dexie，拆分右侧面板

---
+++ pages/components/RightPanel.vue
@@ -0,0 +1,169 @@
+<script setup lang="ts">
+import { number } from 'vue-types'
+import Draggable from 'vuedraggable'
+import { cloneDeep } from 'lodash-es'
+import { dbService } from '~/dexie/dbService'
+import type { GroupType, ListType, TaskType } from '~/types'
+
+const props = defineProps({
+  listId: number().def(NaN),
+})
+
+const list = ref<ListType | null>()
+const isList = computed(() => list.value?.showingMode === 'list')
+const isBoard = computed(() => list.value?.showingMode === 'board')
+
+watchEffect(() => queryUpdateList())
+
+function queryUpdateList() {
+  if (!props.listId) {
+    list.value = null
+  }
+  else {
+    dbService.getListById(props.listId).then((queryList) => {
+      list.value = queryList!
+    })
+  }
+}
+
+async function updateList(newList: ListType) {
+  /** clone，因为groups是reactive数据 */
+  await dbService.updateList(cloneDeep(newList))
+  queryUpdateList()
+}
+
+function handleSwitchModeChange(val: boolean) {
+  const newList: ListType = {
+    ...list.value!,
+    showingMode: val ? 'board' : 'list',
+  }
+  updateList(newList)
+}
+function handleAddTask(task: TaskType) {
+  // TODO: 统一调配添加task
+}
+function handleAddGroup() {
+  handleGroupsChange([
+    ...list.value!.groups,
+    createGroup(),
+  ])
+}
+function handleGroupChange(group: GroupType, index: number) {
+  handleGroupsChange([
+    ...list.value!.groups.slice(0, index),
+    group,
+    ...list.value!.groups.slice(index + 1),
+  ])
+}
+function handleInsertLeftGroup(currentIndex: number) {
+  handleGroupsChange([
+    ...list.value!.groups.slice(0, currentIndex),
+    createGroup(),
+    ...list.value!.groups.slice(currentIndex),
+  ])
+}
+function handleInsertRightGroup(currentIndex: number) {
+  handleGroupsChange([
+    ...list.value!.groups.slice(0, currentIndex + 1),
+    createGroup(),
+    ...list.value!.groups.slice(currentIndex + 1),
+  ])
+}
+function handleDeleteGroup(currentIndex: number) {
+  handleGroupsChange([
+    ...list.value!.groups.slice(0, currentIndex),
+    ...list.value!.groups.slice(currentIndex + 1),
+  ])
+}
+function handleFirstGroupTaskIdsChange(taskIds: number[]) {
+  handleGroupsChange([
+    {
+      ...list.value!.groups[0],
+      taskIds,
+    },
+  ])
+}
+function handleGroupsChange(groups: GroupType[]) {
+  const newList: ListType = {
+    ...list.value!,
+    groups,
+  }
+  updateList(newList)
+}
+</script>
+
+<template>
+  <main v-if="!listId" class="h-100vh box-border">
+    没有数据
+  </main>
+  <main
+    v-else
+    class="p-2 h-100vh box-border bg-gray-1 grid gap-2"
+    :class="{
+      'grid-rows-[auto_auto_1fr]': isList,
+      'grid-rows-[auto_1fr]': isBoard,
+    }"
+  >
+    <header>
+      {{ isBoard ? '看板模式' : '清单模式' }}
+      <NSwitch :value="isBoard" :on-update-value="handleSwitchModeChange" />
+    </header>
+
+    <TaskInput v-if="isList" @add-task="handleAddTask" />
+
+    <template v-if="isList && list?.groups.length === 1">
+      <TaskList :task-ids="list.groups[0].taskIds" @update:task-ids="handleFirstGroupTaskIdsChange" />
+    </template>
+
+    <template v-else-if="isList && list!.groups.length > 1">
+      <Draggable
+        :model-value="list!.groups"
+        class="overflow-y-scroll"
+        item-key="index"
+        tag="n-collapse"
+        :component-data="{
+          themeOverrides: {
+            dividerColor: '#6B6B77FF',
+          },
+          defaultExpandedNames: list!.groups.map((item, index) => `${index}-${item.title}`),
+        }"
+        @update:model-value="handleGroupsChange"
+      >
+        <template #item="{ element, index }">
+          <CollapseGroup
+            :group="element" preset="operate-group" task-list-group="collaspe-group"
+            :collapse-item-name="`${index}-${element.title}`"
+            @update:group="(val) => handleGroupChange(val, index)"
+            @insert-top-group="handleInsertLeftGroup(index)"
+            @insert-bottom-group="handleInsertRightGroup(index)"
+            @delete-group="handleDeleteGroup(index)"
+          />
+        </template>
+      </Draggable>
+    </template>
+
+    <template v-else-if="isBoard">
+      <Draggable
+        :model-value="list!.groups"
+        class="flex items-start gap-2 flex-nowrap overflow-x-scroll scrollbar"
+        item-key="index"
+        @update:model-value="handleGroupsChange"
+      >
+        <template #item="{ element, index }">
+          <Group
+            :group="element" task-list-group="list-group" class="min-w-60 max-w-60"
+            @update:group="(val) => handleGroupChange(val, index)"
+            @insert-left-group="handleInsertLeftGroup(index)"
+            @insert-right-group="handleInsertRightGroup(index)"
+            @delete-group="handleDeleteGroup(index)"
+          />
+        </template>
+        <template #footer>
+          <NButton type="primary" @click="handleAddGroup">
+            添加分组
+          </NButton>
+        </template>
+      </Draggable>
+    </template>
+  </main>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-import Draggable from 'vuedraggable'
-import { storeToRefs } from 'pinia'
-import type { GroupType, TaskType } from '~/types'
-import { useListStore } from '~/stores/useListStore'
 import LeftSider from './components/LeftSider.vue'
+import RightPanel from './components/RightPanel.vue'
 import { dbService } from '~/dexie/dbService'
 
-const { list, isBoard, isList } = storeToRefs(useListStore())
-const listData=useLocalStorage<{
-  listIds:number[],
-  currentListId:number
-}>('listData',{
-  listIds:[],
-  currentListId:NaN
+const listData = useLocalStorage<{
+  listIds: number[]
+  currentListId: number
+}>('listData', {
+  listIds: [],
+  currentListId: NaN,
 })
 
 function handleAddList() {
@@ -20,115 +16,20 @@ function handleAddList() {
     listData.value.listIds.push(newList.id!)
   })
 }
-
-function handleAddGroup() {
-  list.value.groups.push(createGroup())
-}
-
-function handleGroupChange(group: GroupType, index: number) {
-  list.value.groups[index] = group
-}
-
-function handleAddTask(task: TaskType) {
-  addTask2List(list.value, task)
-}
-
-function handleInsertLeftGroup(currentIndex: number) {
-  list.value.groups.splice(currentIndex, 0, createGroup())
-}
-
-function handleInsertRightGroup(currentIndex: number) {
-  list.value.groups.splice(currentIndex + 1, 0, createGroup())
-}
-
-function handleDeleteGroup(currentIndex: number) {
-  list.value.groups.splice(currentIndex, 1)
-}
-
-function handleSwitchModeChange(val: boolean) {
-  list.value.showingMode = val ? 'board' : 'list'
-}
 </script>
 
 <template>
   <NLayout has-sider>
     <NLayoutSider show-trigger="arrow-circle" bordered collapse-mode="transform">
-      <LeftSider 
-        :list-ids="listData.listIds" 
-        v-model:currentListId="listData.currentListId" 
-        @add-list="handleAddList"/>
+      <LeftSider
+        v-model:currentListId="listData.currentListId"
+        :list-ids="listData.listIds"
+        @add-list="handleAddList"
+      />
     </NLayoutSider>
 
     <NLayoutContent>
-      <main
-        class="p-2 h-100vh box-border bg-gray-1 grid gap-2" :class="{
-          'grid-rows-[auto_auto_1fr]': isList,
-          'grid-rows-[auto_1fr]': isBoard,
-        }"
-      >
-        <header>
-          {{ isBoard ? '看板模式' : '清单模式' }}
-          <NSwitch :value="isBoard" :on-update-value="handleSwitchModeChange" />
-        </header>
-
-        <TaskInput v-if="isList" @add-task="handleAddTask" />
-
-        <template v-if="isList && list.groups.length === 1">
-          <TaskList v-model:task-ids="list.groups[0].taskIds" />
-        </template>
-
-        <template v-else-if="isList && list.groups.length > 1">
-          <Draggable
-            v-model="list.groups"
-            class="overflow-y-scroll"
-            item-key="index" tag="n-collapse"
-            :component-data="{
-              themeOverrides: {
-                dividerColor: '#6B6B77FF',
-              },
-              defaultExpandedNames: list.groups.map((item, index) => `${index}-${item.title}`),
-            }"
-          >
-            <template #item="{ element, index }">
-              <CollapseGroup
-                :group="element"
-                preset="operate-group"
-                task-list-group="collaspe-group"
-                :collapse-item-name="`${index}-${element.title}`"
-                @update:group="(val) => handleGroupChange(val, index)"
-                @insert-top-group="handleInsertLeftGroup(index)"
-                @insert-bottom-group="handleInsertRightGroup(index)"
-                @delete-group="handleDeleteGroup(index)"
-              />
-            </template>
-          </Draggable>
-        </template>
-
-        <template v-else-if="isBoard">
-          <Draggable
-            v-model="list.groups"
-            class="flex items-start gap-2 flex-nowrap overflow-x-scroll scrollbar"
-            item-key="index"
-          >
-            <template #item="{ element, index }">
-              <Group
-                :group="element"
-                task-list-group="list-group"
-                class="min-w-60 max-w-60"
-                @update:group="(val) => handleGroupChange(val, index)"
-                @insert-left-group="handleInsertLeftGroup(index)"
-                @insert-right-group="handleInsertRightGroup(index)"
-                @delete-group="handleDeleteGroup(index)"
-              />
-            </template>
-            <template #footer>
-              <NButton type="primary" @click="handleAddGroup">
-                添加分组
-              </NButton>
-            </template>
-          </Draggable>
-        </template>
-      </main>
+      <RightPanel :list-id="listData.currentListId" />
     </NLayoutContent>
   </NLayout>
 </template>
