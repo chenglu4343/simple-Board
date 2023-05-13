b4a1e73df7724091f3b06d12ef03d002e6ba7f82
chenglu <chenglu4343@qq.com>
2023-05-13 14:56:33 +0800

    ✨ feat: 支持boardItem拖拽排序

--- pages/components/LeftSider.vue
+++ pages/components/LeftSider.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { cloneDeep } from 'lodash-es'
 import { storeToRefs } from 'pinia'
+import Draggable from 'vuedraggable'
 import { dbService } from '~/dexie/dbService'
 import { useCurrentBoardStore } from '~/stores/useCurrentBoardStore'
 import { useLocalDataStore } from '~/stores/useLocalDataStore'
@@ -8,9 +9,27 @@ import type { BoardType } from '~/types'
 
 const localListDataStore = useLocalDataStore()
 const { updateBoard } = useCurrentBoardStore()
-const { currentBoardId } = storeToRefs(localListDataStore)
+const { currentBoardId, boardIds } = storeToRefs(localListDataStore)
 
 const boardArr = ref<BoardType[]>([])
+const boardArrModel = computed({
+  get: () => boardArr.value,
+  set: (val) => {
+    const idMap = new Map<number, number>()
+    boardArr.value.forEach((item, index) => {
+      idMap.set(item.id!, val[index].id!)
+    })
+
+    boardArr.value = boardArr.value.map((item) => {
+      const replaceId = idMap.get(item.id!)
+      if (replaceId)
+        return boardArr.value.find(item => item.id === replaceId)!
+
+      return item
+    })
+    boardIds.value = boardArr.value.map(item => item.id!)
+  },
+})
 
 watchEffect(() => updateTaskArr())
 
@@ -45,17 +64,21 @@ async function handleDeleteBoard(board: BoardType, index: number) {
       新建看板
     </NButton>
 
-    <div class="overflow-y-scroll">
-      <BoardItem
-        v-for="(board, index) of boardArr"
-        :key="board.id!"
-        class="mt-2"
-        :board="board"
-        :is-active="currentBoardId === board.id"
-        @update:board="handleUpdateBoard($event, index)"
-        @delete-board="handleDeleteBoard(board, index)"
-        @click="currentBoardId = board.id!"
-      />
-    </div>
+    <Draggable
+      v-model="boardArrModel"
+      class="overflow-y-scroll"
+      item-key="id"
+    >
+      <template #item="{ element, index }">
+        <BoardItem
+          class="mt-2"
+          :board="element"
+          :is-active="currentBoardId === element.id"
+          @update:board="handleUpdateBoard($event, index)"
+          @delete-board="handleDeleteBoard(element, index)"
+          @click="currentBoardId = element.id!"
+        />
+      </template>
+    </Draggable>
   </div>
 </template>
